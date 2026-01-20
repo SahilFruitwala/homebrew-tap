09731174f12889f6c4470671d20a434e22be1b9a
Sahil <31241901+SahilFruitwala@users.noreply.github.com>
2026-01-19 20:21:48 -0600

    v1.0.2

--- Formula/clean-up-clawd.rb
+++ Formula/clean-up-clawd.rb
@@ -1,16 +1,17 @@
 class CleanUpClawd < Formula
   desc "A beautiful terminal-based application for cleaning up directories"
+  license "GPL-3.0-only"
   # TODO: Replace with your actual repository URL
   homepage "https://github.com/sahil/clean-up-clawd"
   
   # TODO: Replace with the actual URL to your release file
   # Example: https://github.com/sahil/clean-up-clawd/releases/download/v1.0.0/clean-up-clawd-v1.0.0-macos.tar.gz
-  url "https://github.com/SahilFruitwala/clean-up-clawd/releases/download/v1.0.1/clean-up-clawd-v1.0.1-macos.tar.gz"
+  url "https://github.com/SahilFruitwala/clean-up-clawd/releases/download/v1.0.2/clean-up-clawd-v1.0.2-macos.tar.gz"
   
   # TODO: Replace with the SHA256 output from homebrew/build_release.sh
-  sha256 "bb9b6db2f072b934f46568195e7d59a42fe0ac6dc458acfe520066c63a39dfdb"
+  sha256 "577879f92c0efcddf5b624aa1b9eccc8ded6bed31e88b2ac3d75c0e39e8c032f"
   
-  version "1.0.1"
+  version "1.0.2"
 
   def install
     # Install the entire directory to libexec
