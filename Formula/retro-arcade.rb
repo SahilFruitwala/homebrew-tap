c3389ecef98f32c4859addef79f4c54b7a02aa88
Sahil <31241901+SahilFruitwala@users.noreply.github.com>
2026-02-06 10:18:50 -0500

    Update URL and SHA256 for version 2.0.0

--- Formula/retro-arcade.rb
+++ Formula/retro-arcade.rb
@@ -1,8 +1,8 @@
 class RetroArcade < Formula
   desc "Terminal-based arcade collection (Space Invaders, Snake, 2048, flappy bird)"
   homepage "https://github.com/SahilFruitwala/retro-arcade"
-  url "https://github.com/SahilFruitwala/retro-arcade/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "ef448a233602d59d809fdaa00ac9a63146d30671fcf716f1c913cd492a254f49"
+  url "https://github.com/SahilFruitwala/retro-arcade/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "1df4a3b05fa5ac687cd3fb61cb68501d691661ac1e61ec5846b15d8e2c0c7b61"
   license "MIT"
 
   depends_on "oven-sh/bun/bun" => :build
