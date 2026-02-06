4e81d36db8ca5a87cb07e2f70511a5dbba24daad
Sahil <31241901+SahilFruitwala@users.noreply.github.com>
2026-02-05 23:25:31 -0500

    Update description and version for RetroArcade

--- Formula/retro-arcade.rb
+++ Formula/retro-arcade.rb
@@ -1,8 +1,8 @@
 class RetroArcade < Formula
-  desc "Terminal-based arcade collection (Space Invaders, Snake)"
+  desc "Terminal-based arcade collection (Space Invaders, Snake, 2048, flappy bird)"
   homepage "https://github.com/SahilFruitwala/retro-arcade"
-  url "https://github.com/SahilFruitwala/retro-arcade/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "e63c190ed108bfe080b51bc45fbf7771cd8debcf754b4e0ff11f71354b4a6b55"
+  url "https://github.com/SahilFruitwala/retro-arcade/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "ef448a233602d59d809fdaa00ac9a63146d30671fcf716f1c913cd492a254f49"
   license "MIT"
 
   depends_on "oven-sh/bun/bun" => :build
